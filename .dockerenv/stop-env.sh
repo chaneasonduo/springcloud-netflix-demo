51821ec096fe54af85543a8d75b21e1353d055be
HairstyleEason <wpppppps@163.com>
2025-06-21 13:29:53 +0800

    部署配置；性能监控

--- .dockerenv/stop-env.sh
+++ .dockerenv/stop-env.sh
@@ -1,3 +1,25 @@
-#!bin/bash
+#!/bin/bash
+
+
+# 封装成函数，获取项目路径
+get_project_path() {
+    # 获取脚本的绝对路径
+    local SCRIPT_PATH=$(readlink -f "$0")
+    # 获取脚本的父路径（项目路径）
+    local PROJECT_PATH=$(dirname $(dirname "$SCRIPT_PATH"))
+    # 输出项目路径，或者你可以选择不输出，而是直接返回路径
+    echo "$PROJECT_PATH"
+}
+
+# 调用函数获取项目路径，并赋值给变量
+PROJECT_PATH=$(get_project_path)
+
+# 输出项目路径
+echo "project path: $PROJECT_PATH"
+
+cd $PROJECT_PATH || exit
+
+echo "pwd: $(pwd)"
+
 cd /workspaces/springcloud-netflix-demo
 docker-compose -f .dockerenv/docker-compose.all.yml down
